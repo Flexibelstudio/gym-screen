4fa8111e2054f2d7dc01435ed3bc295f9c7517cc
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2026-06-08 15:51:38 +0200

    fix

--- public/sw.js
+++ public/sw.js
@@ -6,8 +6,8 @@
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js");
 
-const STATIC_CACHE_NAME = 'smart-skarm-static-v11';
-const DYNAMIC_CACHE_NAME = 'smart-skarm-dynamic-v11';
+const STATIC_CACHE_NAME = 'smart-skarm-static-v12';
+const DYNAMIC_CACHE_NAME = 'smart-skarm-dynamic-v12';
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDkdGj5XOkTdpfhSpcvO_TwDTjy204qTq0",
   authDomain: "gym-screen.firebaseapp.com",
