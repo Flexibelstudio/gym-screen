f23fa0df895fbf94c36431ed85f7c8d0067d12e7
Karin Birgitta <karin@flexibelfriskvardhalsa.se>
2026-08-12 16:57:15 +0200

    fix

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,7 @@ const FIREBASE_CONFIG = {
   apiKey: "AIzaSyDkdGj5XOkTdpfhSpcvO_TwDTjy204qTq0",
   authDomain: "gym-screen.firebaseapp.com",
   projectId: "gym-screen",
-  storageBucket: "gym-screen.fir053ebasestorage.app",
+  storageBucket: "gym-screen.firebasestorage.app",
   messagingSenderId: "970379052933",
   appId: "1:970379052933:web:0756911ee508d5c8c1a6ec"
 };
